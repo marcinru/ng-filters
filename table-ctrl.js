d247d877fe785f28a854dd8ad19dc78fbba9938d
Marcin Rudzki <mar.rudzki@gmail.com>
2016-03-13 22:03:11 +0100

    naming and quote marks alignment

--- table-ctrl.js
+++ table-ctrl.js
@@ -1,7 +1,7 @@
 angular.module('filters', []).controller('countriesCtrl', function($http) {
     var countries = this;
     countries.list = [];
-    $http.get("countries.json").then(function(response) {
+    $http.get('countries.json').then(function(response) {
         countries.list = response.data;
     });
     countries.sortOrderAsc = true;
