f27d0deeda3147052ef27095247a192e03395565
Marcin Rudzki <mar.rudzki@gmail.com>
2016-03-13 21:51:04 +0100

    Adding toggleSortOrder method

--- table-ctrl.js
+++ table-ctrl.js
@@ -1,8 +1,13 @@
-angular.module('filters', [])
-    .controller('countriesCtrl', function($http) {
+angular.module('filters', []).controller('countriesCtrl', function($http) {
     var countries = this;
     countries.list = [];
     $http.get("countries.json").then(function(response) {
         countries.list = response.data;
     });
+    countries.sortOrderAsc = true;
+    countries.sortOrder = 'name';
+        countries.toggleSortOrder = function() {
+        countries.sortOrder = countries.sortOrderAsc ? '-name' : 'name';
+        countries.sortOrderAsc = !countries.sortOrderAsc;
+    }
 });
