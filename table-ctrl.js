d19a302d17d3bce615d3b67098cdf8682260e042
Marcin Rudzki <mar.rudzki@gmail.com>
2016-03-13 11:58:39 +0100

    Refactoring of names and indents

--- table-ctrl.js
+++ table-ctrl.js
@@ -1,7 +1,8 @@
-angular.module('countries', []).controller('countriesCtrl', function($http) {
-	var countries = this;
-	countries.list = [];
-	$http.get("countries.json").then(function(response) {
-		countries.list = response.data;
-	});
+angular.module('filters', [])
+    .controller('countriesCtrl', function($http) {
+    var countries = this;
+    countries.list = [];
+    $http.get("countries.json").then(function(response) {
+        countries.list = response.data;
+    });
 });
